af038e42b30aeacbfeb425dd53d6deae99a59827
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-06-28 14:27:30 +0800

    ch10

--- ch03_multi_class.py
+++ ch03_multi_class.py
@@ -29,24 +29,59 @@ one_hot_train_labels = to_one_hot(train_labels)
 one_hot_test_labels = to_one_hot(test_labels)
 
 
+# Building Models p.85
+from keras import layers
+from keras import models
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000, )))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Let’s set apart 1,000 samples in our training data to use as a validation set: (p.86)
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+import matplotlib.pyplot as plt
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, 'bo')
+plt.plot(epochs, val_loss_values, 'b+')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.show()
 
 
+# Re-training a model from scratch (p.88)
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
 
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
+model.fit(x_train,
+          one_hot_train_labels,
+          epochs=9,
+          batch_size=512,
+          validation_data=(x_test, one_hot_test_labels))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+results = model.evaluate(x_test, one_hot_test_labels)
+print(results)
